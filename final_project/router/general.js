7b9c400fe1a6d6b30989b2146b510e176b56dcea
Gustavo López Mendoza <tavolopezmendoza@gmail.com>
2024-03-01 03:57:22 -0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,8 +31,10 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.titler;
+    const bookTitle = Object.values(books);
+    const book = bookTitle.filter((book) => book.title === title);
+    res.status(200).json(book);
 });
 
 //  Get book review
