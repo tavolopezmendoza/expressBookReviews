81e5875fb222129b239c45514ae6f4e65025d1d2
Gustavo López Mendoza <tavolopezmendoza@gmail.com>
2024-03-04 02:05:31 -0300

    Update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,29 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+const doesExist = (username) => {
+    let userwithsamename = users.filter((user) => {
+        return user.name === username
+    });
+    if (userwithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ mesage: "User successfully registered." });
+        } else {
+            return res.status(404).json({message: "User already exists."})
+        }
+    }
+    return res.status(404).json({message: "Unable to register user."})
 });
 
 // Get the book list available in the shop
@@ -39,8 +58,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].review)
 });
 
 module.exports.general = public_users;
